b5647c956774eb7a5e200baf49c8c5d3d59ade90
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-03-20 20:14:31 +0900

    Title: 총점 비교, Time: 236ms, Memory: 14MB, Status: Passed - Codetree

---
+++ 260320/총점 비교/compare-total-points.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Person {
+        String name;
+        int k, e, m;
+
+        Person(String name, int k, int e, int m) {
+            this.name = name;
+            this.k = k;
+            this.e = e;
+            this.m = m;
+        }
+
+        public String toString() {
+            return name + " " + k + " " + e + " " + m + "\n";
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        Person[] arr = new Person[n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int k = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            arr[i] = new Person(name, k, e, m);
+        }
+        
+        Arrays.sort(arr, (a, b) -> {
+            return (a.k + a.e + a.m) - (b.k + b.e + b.m);
+        });
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+        }
+
+        System.out.println(sb);
+    }
+}
