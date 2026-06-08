5ed1a95f215db88c58f8a102947b644bb4161a2f
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-08 18:55:09 +0900

    [Passed] 요일 맞추기 (89ms, 9MB)

---
+++ 260608/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final int[] NUM_OF_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String[] DAY_OF_MONTH = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        int dayOfMonth = 0; // 월요일
+        int diff = convertToDays(m2, d2) - convertToDays(m1, d1);
+
+        if (diff < 0) {
+            diff = convertToDays(m1, d1) - convertToDays(m2, d2);
+            diff %= 7;
+            dayOfMonth = (7 - diff) % 7;
+        } else {
+            diff %= 7;
+            dayOfMonth += diff;
+        }
+        System.out.println(DAY_OF_MONTH[dayOfMonth]);
+    }
+
+    private static int convertToDays(int m, int d) {
+        int total = 0;
+        for (int i = 1; i < m; i++) {
+            total += NUM_OF_DAYS[i];
+        }
+        total += d;
+        return total;
+    }
+}
