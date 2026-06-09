edbe0d3eb7d8173c4d0547bbf644e828ec147f36
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-09 21:00:33 +0900

    [Passed] 최대공약수 구하기 (145ms, 10MB)

--- 260609/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 260609/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -5,19 +5,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        System.out.println(findGCD(n, m));
+    }
+
+    private static int getGCD(int n, int m) {
         // n m
         // 12 18
         // 18 12
         // 12  6
         // 6  6
         // 6 0
-        System.out.println(getGCD(n, m));
-    }
-
-    private static int getGCD(int n, int m) {
         if (m == 0) {
             return n;
         }
         return getGCD(m, n % m); // 무조건 n > m 형태
+    }
+
+    private static int findGCD(int n, int m) {
+        int gcd = 1;
+        for (int i = 1; i <= Math.min(n, m); i++) {
+            if (n % i == 0 && m % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
     }
 }
