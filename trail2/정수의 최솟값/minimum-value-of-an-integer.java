c746cc7018faf58d8312fb3ee4d50e1e4d6013fd
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-17 21:13:33 +0900

    [Passed] 정수의 최솟값 (127ms, 10MB)

---
+++ trail2/정수의 최솟값/minimum-value-of-an-integer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(findMin(a, b, c));
+    }
+
+    private static int findMin(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
