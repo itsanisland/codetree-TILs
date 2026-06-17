4cef8f2b2275f9689b7a158fe210f648690536ef
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-17 21:34:05 +0900

    [Passed] 함수를 이용한 소수 판별 (130ms, 10MB)

--- trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     private static boolean isPrime(int n) {
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 return false;
             }
