6be959cdff965d0700ac405a5ef5be4b3f1ce6a1
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-17 21:28:37 +0900

    [Passed] 함수를 이용한 369 게임 (139ms, 10MB)

--- trail2/함수를 이용한 369 게임/369-games-using-functions.java
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.java
@@ -7,15 +7,24 @@ public class Main {
         int b = sc.nextInt();
         int cnt = 0;
         for (int i = a; i <= b; i++) {
-            if (i % 3 == 0 || checkNum(i)) {
+            if (i % 3 == 0 || contains369(i)) {
                 cnt++;
             }
         }
         System.out.println(cnt);
     }
 
-    private static boolean checkNum(int n) {
-        String str = "" + n;
-        return str.contains("3") || str.contains("6") || str.contains("9");
+    private static boolean contains369(int n) {
+        // 1 0 3 4 5
+        // <-------- 이 방향으로 조사
+        while (n > 0) {
+            // 일의 자리 조사
+            if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) {
+                return true;
+            }
+            // 계속 10으로 나눔
+            n /= 10;
+        }
+        return false;
     }
 }
