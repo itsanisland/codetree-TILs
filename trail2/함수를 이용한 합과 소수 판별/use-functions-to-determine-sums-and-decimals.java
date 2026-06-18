7b71de75ef7d90dbc22c4c3e5f084f3db18be6a3
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-18 17:04:30 +0900

    [Passed] 함수를 이용한 합과 소수 판별 (130ms, 10MB)

---
+++ trail2/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i) && checkEven(i)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    private static boolean checkEven(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum % 2 == 0;
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
