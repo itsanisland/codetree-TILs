ff47bbc9ed736bfca93bc6ae76e11d3727d0a670
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-21 18:17:32 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (131ms, 10MB)

---
+++ trail2/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+
+    private static int solve(int n) {
+        if (n == 1) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return solve(n / 2) + 1;
+        } else {
+            return solve(n * 3 + 1) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solve(n));
+    }
+}
