d98219e1dde4cec80750d4d5ba4c2746df335818
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-22 18:09:24 +0900

    [Passed] 문자열 정렬 (93ms, 9MB)

---
+++ trail2/문자열 정렬/string-sort.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        String str = new String(arr);
+
+        System.out.println(str);
+    }
+}
