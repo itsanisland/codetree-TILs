c378586afc332422dae580bb893bbf72265f9e4f
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-22 19:33:43 +0900

    [Passed] k번째로 신기한 문자열 (99ms, 9MB)

---
+++ trail2/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        String t = st.nextToken();
+        List<String> list = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            if (s.length() < t.length()) continue;
+
+            boolean isPass = true;
+            for (int j = 0; j < t.length(); j++) {
+                if (s.charAt(j) != t.charAt(j)) {
+                    isPass = false;
+                    break;
+                }
+            }
+
+            if (isPass) {
+                list.add(s);
+            }
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(k - 1));
+    }
+}
