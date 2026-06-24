483e39cc63da651b250c5e10fd8cef98559f0e54
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-24 18:59:32 +0900

    [Passed] 여러가지 진수변환 (128ms, 10MB)

---
+++ trail2/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        
+        while (n > 0) {
+            sb.append(n % b);
+            n /= b;
+        }
+
+        System.out.println(sb.reverse());
+    }
+}
