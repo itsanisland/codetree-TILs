a04fe4343a58958411211a2d833d83630f8e2864
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-24 19:51:45 +0900

    [Passed] 왔다 갔던 구역 2 (95ms, 9MB)

--- trail2/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -2,34 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static final int OFFSET = 100;
+    private static final int MAX_R = 200;
+    private static final int MAX_N = 100;
+
+    private static int n;
+    private static int[] x1 = new int[MAX_N]; // 시작 지점
+    private static int[] x2 = new int[MAX_N]; // 끝 지점
+
+    private static int[] checked = new int[MAX_R + 1];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[201];
-        int cur = 100;
+        n = Integer.parseInt(st.nextToken());
+        int cur = 0;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             char cmd = st.nextToken().charAt(0);
+            
             if (cmd == 'R') {
-                for (int j = 0; j < x; j++) {
-                    arr[cur + j]++;
-                }
+                x1[i] = cur;
+                x2[i] = cur + x;
                 cur += x;
             } else {
-                for (int j = 1; j <= x; j++) {
-                    arr[cur - j]++;
-                }
+                // 왼쪽으로 이동할 경우: (cur - x) ~ cur 까지 경로 이돈
+                x1[i] = cur - x;
+                x2[i] = cur;
                 cur -= x;
+            }
+
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
+        }
+
+        // 구간 칠하기
+        for (int i = 0; i < n; i++) {
+            for (int j = x1[i]; j < x2[i]; j++) {
+                checked[j]++;
             }
         }
 
         int ans = 0;
         for (int i = 0; i <= 200; i++) {
-            if (arr[i] >= 2) {
+            if (checked[i] >= 2) {
                 ans++;
             }
         }
