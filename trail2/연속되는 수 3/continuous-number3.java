bb0d0df89ed23a90e693f27ef4459286082622f1
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-25 18:42:39 +0900

    [Passed] 연속되는 수 3 (163ms, 11MB)

---
+++ trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int len = 1;
+        int ans = 0;
+        int prev = 0;
+
+        while (n-- > 0) {
+            int input = sc.nextInt();
+            if (prev == 0 || prev * input < 0) {
+                len = 1;
+            } else {
+                len++;
+            }
+            prev = input;
+            ans = Math.max(ans, len);
+        }
+
+        System.out.println(ans);
+    }
+}
