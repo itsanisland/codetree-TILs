5cf11aadff74e15d8162de9a38aabeeb0004c598
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-26 19:18:15 +0900

    [Passed] 좌우로 움직이는 로봇 (422ms, 38MB)

---
+++ trail2/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final int MAX_T = 2_000_000;
+
+    private static int[] posA = new int[MAX_T + 1];
+    private static int[] posB = new int[MAX_T + 1];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int curA = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            char d = st.nextToken().charAt(0);
+
+            if (d == 'L') {
+                while (t-- > 0) {
+                    posA[curA + 1] = posA[curA] - 1;
+                    curA++;
+                }
+            } else {
+                while (t-- > 0) {
+                    posA[curA + 1] = posA[curA] + 1;
+                    curA++;
+                }
+            }
+        }
+
+        int curB = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            char d = st.nextToken().charAt(0);
+
+            if (d == 'L') {
+                while (t-- > 0) {
+                    posB[curB + 1] = posB[curB] - 1;
+                    curB++;
+                }
+            } else {
+                while (t-- > 0) {
+                    posB[curB + 1] = posB[curB] + 1;
+                    curB++;
+                }
+            }
+        }
+
+        int ans = 0;
+        boolean isSamePos = true;
+        for (int i = 1; i <= Math.max(curA, curB); i++) {
+            posA[i] = i > curA ? posA[curA] : posA[i];
+            posB[i] = i > curB ? posB[curB] : posB[i];
+            if (posA[i] == posB[i]) {
+                if (!isSamePos) {
+                    ans++;
+                }
+            }
+            isSamePos = posA[i] == posB[i];
+        }
+
+        System.out.println(ans);
+    }
+}
