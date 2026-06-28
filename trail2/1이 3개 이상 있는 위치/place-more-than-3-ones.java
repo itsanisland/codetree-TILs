0fd18d310f5c7aa8862325b907926b7a2cc99f35
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-28 21:27:38 +0900

    [Passed] 1이 3개 이상 있는 위치 (256ms, 18MB)

--- trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
 public class Main {
 
+    private static final int MAX_N = 100;
     private static final int[] DX = {-1, 1, 0, 0}; // 행
     private static final int[] DY = {0, 0, -1, 1}; // 열
 
     private static int n;
+    private static int[][] arr = new int[MAX_N][MAX_N];
 
     private static boolean inRange(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < n;
     }
 
+    private static int adjacentCnt(int x, int y) {
+        int cnt = 0;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + DX[d];
+            int ny = y + DY[d];
+            if (inRange(nx, ny) && arr[nx][ny] == 1) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int[][] arr = new int[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
@@ -23,15 +37,7 @@ public class Main {
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int cnt = 0;
-                for (int d = 0; d < 4; d++) {
-                    int x = i + DX[d];
-                    int y = j + DY[d];
-                    if (inRange(x, y) && arr[x][y] == 1) {
-                        cnt++;
-                    }
-                }
-                if (cnt >= 3) {
+                if (adjacentCnt(i, j) >= 3) {
                     ans++;
                 }
             }
