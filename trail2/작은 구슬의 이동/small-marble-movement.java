8519662fb560991d437d99e649e2c76b12330883
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-29 21:54:51 +0900

    [Passed] 작은 구슬의 이동 (92ms, 9MB)

--- trail2/작은 구슬의 이동/small-marble-movement.java
+++ trail2/작은 구슬의 이동/small-marble-movement.java
@@ -3,51 +3,50 @@ import java.util.*;
 
 public class Main {
 
+    private static final int ASCII_NUM = 128;
+    private static final int DIR_NUM = 4;
+
     private static final int[] DX = {-1, 0, 0, 1};
     private static final int[] DY = {0, -1, 1, 0};
 
-    private static int n;
-
-    private static int getDir(char d) {
-        switch (d) {
-            case 'U':
-                return 0;
-            case 'D':
-                return 3;
-            case 'R':
-                return 2;
-            case 'L':
-                return 1;
-        }
-        return -1;
-    }
+    private static int n, t;
+    private static int x, y, dir;
+    private static int[] mapper = new int[ASCII_NUM];
 
     private static boolean inRange(int x, int y) {
         return x >= 1 && x <= n && y >= 1 && y <= n;
+    }
+
+    private static void simulate() {
+        while (t-- > 0) {
+            if (inRange(x + DX[dir], y + DY[dir])) {
+                x += DX[dir];
+                y += DY[dir];
+            } else {
+                dir = 3 - dir;
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        int t = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        char d = st.nextToken().charAt(0);
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
 
-        int dir = getDir(d);
+        mapper['U'] = 0;
+        mapper['L'] = 1;
+        mapper['R'] = 2;
+        mapper['D'] = 3;
 
-        while (t-- > 0) {
-            if (inRange(r + DX[dir], c + DY[dir])) {
-                r += DX[dir];
-                c += DY[dir];
-            } else {
-                dir = 3 - dir;
-            }
-        }
+        dir = mapper[st.nextToken().charAt(0)];
 
-        System.out.println(r + " " + c);
+        simulate();
+
+        System.out.println(x + " " + y);
     }
 }
