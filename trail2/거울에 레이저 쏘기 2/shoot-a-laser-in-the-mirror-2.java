1172b5ff2180edde97403182f1fb5071e9fd2407
SEO MIJI <57468832+itsanisland@users.noreply.github.com>
2026-06-30 20:58:58 +0900

    [Passed] 거울에 레이저 쏘기 2 (98ms, 11MB)

--- trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -7,11 +7,50 @@ public class Main {
     private static final int[] DX = {1, 0, -1, 0}; // 하 좌 상 우
     private static final int[] DY = {0, -1, 0, 1};
 
-    private static int n, k;
+    private static int n, k, x, y, dir;
     private static char[][] map = new char[MAX_N][MAX_N];
     
     private static boolean inRange(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < n;
+    }
+
+    private static void initialize() {
+        if (k <= n) {
+            x = 0;
+            y = k - 1;
+            dir = 0;
+        } else if (k <= 2 * n) {
+            x = k - n - 1;
+            y = n - 1;
+            dir = 1;
+        } else if (k <= 3 * n) {
+            x = n - 1;
+            y = n - (k - 2 * n);
+            dir = 2;
+        } else {
+            x = n - (k - 3 * n);
+            y = 0;
+            dir = 3;
+        }
+    }
+
+    private static void move(int nextDir) {
+        x += DX[nextDir];
+        y += DY[nextDir];
+        dir = nextDir;
+    }
+
+    private static int simulate() {
+        int moveNum = 0;
+        while (inRange(x, y)) {
+            if (map[x][y] == '/') {
+                move(dir ^ 1);
+            } else {
+                move(3 - dir);
+            }
+            moveNum++;
+        }
+        return moveNum;
     }
 
     public static void main(String[] args) throws IOException {
@@ -26,52 +65,10 @@ public class Main {
         k = Integer.parseInt(br.readLine());
 
         // k를 통해 초기 위치, 방향 확인
-        int x = 0, y = 0;
-
-        int tempDir = 3;
-        for (int i = 1; i < k; i++) {
-            int nx = x + DX[tempDir];
-            int ny = y + DY[tempDir];
-
-            if (inRange(nx, ny)) {
-                x = nx;
-                y = ny;
-            } else {
-                tempDir = (tempDir + 1) % 4;
-            }
-        }
-
-        int dir = (k - 1) / n;
+        initialize();
 
         // 거울 빛 튕기기
-        int ans = 0;
-        while (inRange(x, y)) {
-            ans++;
-            if (map[x][y] == '\\') {
-                if (dir == 0) {
-                    dir = 3;
-                } else if (dir == 1) {
-                    dir = 2;
-                } else if (dir == 2) {
-                    dir = 1;
-                } else {
-                    dir = 0;
-                }
-            } else {
-                if (dir == 0) {
-                    dir = 1;
-                } else if (dir == 1) {
-                    dir = 0;
-                } else if (dir == 2) {
-                    dir = 3;
-                } else {
-                    dir = 2;
-                }
-            }
-            x += DX[dir];
-            y += DY[dir];
-        }
-
+        int ans = simulate();
         System.out.println(ans);
     }
 }
